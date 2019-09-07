2491599f8cdcabbc0586ffb6274a97acf7b0537d
Bryan <bryansaltzman@gmail.com>
2019-09-07 11:43:00 -0700

    :q

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Vue from "vue";
+import wrap from '@vue/web-component-wrapper';
+import VueWebComponent from './components/VueWebComponent';
+
+const CustomElement = wrap(Vue, VueWebComponent);
+
+window.customElements.define('my-custom-element', CustomElement);
 
 import App from "./App.vue";
 Vue.config.productionTip = false;
@@ -6,10 +12,3 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App)
 }).$mount("#app");
-
-import wrap from '@vue/web-component-wrapper';
-import VueWebComponent from './components/VueWebComponent';
-
-const CustomElement = wrap(Vue, VueWebComponent);
-
-window.customElements.define('my-custom-element', CustomElement);
