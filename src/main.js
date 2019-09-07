1d5bcf075f51fa99823ba48738227cfd8f69a072
Bryan <bryansaltzman@gmail.com>
2019-09-07 10:30:49 -0700

    starting point for a Vue Web Component Project

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+import wrap from '@vue/web-component-wrapper';
+import VueWebComponent from './components/VueWebComponent';
+
+const CustomElement = wrap(Vue, VueWebComponent);
+
+window.customElements.define('my-custom-element', CustomElement);
